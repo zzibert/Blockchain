dc017bff68a9be6083ac821bf05c00734f48dd24
zan <zan_zibert@hotmail.com>
2018-11-26 22:01:25 +0100

    Building a Blockchain in Golang - Part 1

--- go.mod
+++ go.mod
@@ -1,3 +1,1 @@
-module github.com/tensor-programming/golang-blockchain
-
-require rsc.io/quote v1.5.2
+module blockchain
--- main.go
+++ main.go
@@ -30,8 +30,8 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
+	block := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, block)
 }
 
 func Genesis() *Block {
@@ -45,14 +45,14 @@ func InitBlockChain() *BlockChain {
 func main() {
 	chain := InitBlockChain()
 
-	chain.AddBlock("First Block after Genesis")
-	chain.AddBlock("second Block after Genesis")
-	chain.AddBlock("third Block after Genesis")
+	chain.AddBlock("first block after genesis")
+	chain.AddBlock("second block after genesis")
+	chain.AddBlock("third block after genesis")
 
 	for _, block := range chain.blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf(" Hash: %x\n", block.Hash)
+		fmt.Printf("Hash: %x\n", block.Hash)
 	}
 
 }
